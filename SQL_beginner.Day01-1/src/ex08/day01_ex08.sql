2d249acf314284df8ed30ea23cee5275a82b946b
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-08 16:04:44 -0500

    Delete SQL_beginner.Day01-1 directory

--- SQL_beginner.Day01-1/src/ex08/day01_ex08.sql
+++
@@ -1,6 +0,0 @@
-SELECT p_o.order_date AS order_date,
-       person.name || ' (age:' || age || ')' AS person_information
-FROM person_order AS p_o (po_id, id, menu_id, order_date)
-NATURAL JOIN person
-ORDER BY order_date ASC,
-         person_information ASC
