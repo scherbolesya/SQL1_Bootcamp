f3bec28e4da8499e39403fe6fcb9ac876d6d894a
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-15 20:19:13 -0500

    Delete SQL_beginner.Day01-1 directory

--- SQL_beginner.Day01-1/day01_ex01.sql
+++
@@ -1,6 +0,0 @@
-SELECT pizza_name AS object_name
-FROM menu
-UNION ALL
-SELECT name
-FROM person
-ORDER BY object_name
--- SQL_beginner.Day01-1/day01_ex06.sql
+++
@@ -1,7 +0,0 @@
-SELECT DISTINCT person_order.order_date AS action_date,
-                person.name AS person_name
-FROM person_order
-JOIN person_visits ON person_visits.visit_date=person_order.order_date
-JOIN person ON person.id=person_order.person_id
-ORDER BY action_date ASC,
-         person_name DESC
